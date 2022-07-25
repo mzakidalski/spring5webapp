dae930a18fef974ca0e36f911635b2cda0a5ce7b
mzakidalski <mzakidalski@gmail.com>
2022-07-25 23:26:05 +0200

    Added Publisher entity and repository.

--- src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -13,7 +13,7 @@ public class Publisher {
     private Long id;
 
     private String name;
-    private String address;
+    private String addressLine1;
     private String city;
     private String state;
     private String zip;
@@ -21,9 +21,9 @@ public class Publisher {
     public Publisher() {
     }
 
-    public Publisher(String name, String address, String city, String state, String zip) {
+    public Publisher(String name, String addressLine1, String city, String state, String zip) {
         this.name = name;
-        this.address = address;
+        this.addressLine1 = addressLine1;
         this.city = city;
         this.state = state;
         this.zip = zip;
@@ -45,12 +45,12 @@ public class Publisher {
         this.name = name;
     }
 
-    public String getAddress() {
-        return address;
+    public String getAddressLine1() {
+        return addressLine1;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
+    public void setAddressLine1(String address) {
+        this.addressLine1 = address;
     }
 
     public String getCity() {
@@ -82,7 +82,7 @@ public class Publisher {
         return "Publisher{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
-                ", address='" + address + '\'' +
+                ", address='" + addressLine1 + '\'' +
                 ", city='" + city + '\'' +
                 ", state='" + state + '\'' +
                 ", zip='" + zip + '\'' +
